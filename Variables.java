958c802121fb567a04fa6d84e9d832a36beb1843
Mamilla Manoj kumar <93309757+ManojMamilla@users.noreply.github.com>
2022-03-28 19:05:29 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+
+class Variables{
+    static boolean a;
+    static byte b;
+    static short c;
+    static int d;
+    static float e;
+    static char f;
+    static String g;
+
+    public static void main(String s[]){
+        System.out.println("bool : " + a);
+        System.out.println("byte : " + b);
+        System.out.println("short : " + c);
+        System.out.println("int : " + d);
+        System.out.println("float : " + e);
+        System.out.println("char : " + f);
+        System.out.println("string : " + g);
+    }
+}
